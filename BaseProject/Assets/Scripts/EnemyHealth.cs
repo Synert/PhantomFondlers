e0ab0c9a8eb0cf824090fa97dc76951f63893337
jplant20 <Josephsp@hotmail.co.uk>
2017-10-25 17:40:25 +0100

    Created a basic mechanic to change the player's alpha levels to fade them for the invisibility mechanic. Also added some form of damage to the bullets, which now fire in two directions (Fire1 and Fire2).

---
+++ BaseProject/Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public int maxHealth;
+    public int curHealth;
+
+    void Start()
+    {
+
+        curHealth = maxHealth;
+
+    }
+    void Update()
+    {
+        if (curHealth < 1)
+        {
+            Destroy(gameObject);
+        }
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Bullet")
+        {
+            curHealth -= 1;
+            Destroy(col.gameObject);
+        }
+    }
+}
---
+++ BaseProject/Assets/Scripts/Invincibility.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Invincibility : MonoBehaviour
+{
+    public KeyCode InvincibilityKey;
+    public float SpiritLevel = 1.0f;
+    private float currCountdownValue = 0.1f;
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(Input.GetKeyDown (InvincibilityKey))
+        {
+            SpiritLevel -= 0.75f;
+            StartCoroutine(StartCountdown());
+        }
+        if(currCountdownValue == 0)
+        {
+            SpiritLevel += 0.75f;
+        }
+        if(SpiritLevel > 1)
+        {
+            SpiritLevel = 1;
+        }
+
+        GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, SpiritLevel);
+    }
+
+    public IEnumerator StartCountdown(float countdownValue = 10)
+    {
+        currCountdownValue = countdownValue;
+        while (currCountdownValue > 0)
+        {
+            Debug.Log("Countdown: " + currCountdownValue);
+            yield return new WaitForSeconds(1.0f);
+            currCountdownValue--;
+        }
+    }
+}
