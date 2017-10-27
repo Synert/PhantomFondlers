ec22a8001a64a8cbb001e6bf3ce1e1b580830bdc
unknown <benedith97@yahoo.com>
2017-10-27 17:19:21 +0100

    Menu

---
+++ BaseProject/Assets/Scripts/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent (typeof(AudioSource))]
+
+public class Movie : MonoBehaviour {
+    public MovieTexture movie;
+	// Use this for initialization
+	void Start () {
+
+        GetComponent<RawImage>().texture = movie as MovieTexture;
+	}
+	
+
+}
