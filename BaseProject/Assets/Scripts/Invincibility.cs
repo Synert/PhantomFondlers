0b3777d974a2e682f00d564f40149d511e6922ba
Shesawrus <Nicolas2.smith@live.uwe.ac.uk>
2017-10-27 13:31:30 +0100

    Dodging in just needs tweaking for balancing reasons

--- BaseProject/Assets/Scripts/Invincibility.cs
+++ BaseProject/Assets/Scripts/Invincibility.cs
@@ -4,35 +4,103 @@ using UnityEngine;
 
 public class Invincibility : MonoBehaviour
 {
+    public PlayerController playerController;
     public KeyCode InvincibilityKey;
+    public bool isInvincible;
     public float SpiritLevel = 1.0f;
+
     private float currCountdownValue = 0.1f;
+    private float direction;
+    private bool isDogding = false;
+    private Vector2 destitation;
+    private float cooldown = 0f;
+    private float duration = 0f;
+    float originalDistance;
 
     // Use this for initialization
     void Start ()
     {
 		
 	}
-	
-	// Update is called once per frame
-	void Update ()
+
+    // Update is called once per frame
+    void Update()
     {
-        if(Input.GetKeyDown (InvincibilityKey))
-        {
-            SpiritLevel -= 0.75f;
-            StartCoroutine(StartCountdown());
-        }
-        if(currCountdownValue == 0)
+        if (currCountdownValue == 0)
         {
             SpiritLevel += 0.75f;
         }
-        if(SpiritLevel > 1)
+        if (SpiritLevel > 1)
         {
             SpiritLevel = 1;
         }
-
         GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, SpiritLevel);
+
+        if (isDogding)
+        {
+            movement(direction);
+
+        }
+        if (cooldown > 0)
+        {
+            cooldown -= Time.deltaTime;
+        }
+            
+    
     }
+
+    void dogde()
+    {
+        if (GetComponent<Rigidbody2D>().velocity != Vector2.zero)
+        {
+            if (GetComponent<Rigidbody2D>().velocity.x > -playerController.MaxSpeed && cooldown <= 0)
+            {
+                GetComponent<Rigidbody2D>().AddForce(new Vector2(-playerController.Acceleration, 0.0f));
+                Debug.Log("Moving Right");
+                direction = 1;
+                cooldown = 1.0f;
+                duration = 0.8f;
+                destitation = new Vector2((transform.position.x + 5), transform.position.y);
+                originalDistance = Vector2.Distance(transform.position, destitation);
+                playerController.movementPause = true;
+                isDogding = true;
+                isInvincible = true;
+            }
+            if (GetComponent<Rigidbody2D>().velocity.x < playerController.MaxSpeed && cooldown <= 0)
+            {
+                GetComponent<Rigidbody2D>().AddForce(new Vector2(playerController.Acceleration, 0.0f));
+                Debug.Log("Moving Left");
+                direction = -1;
+                cooldown = 1.0f;
+                duration = 0.8f;
+                destitation = new Vector2((transform.position.x - 5), transform.position.y);
+                originalDistance = Vector2.Distance(transform.position, destitation);
+                playerController.movementPause = true;
+                isDogding = true;
+                isInvincible = true;
+            }
+        }
+
+
+        //StartCoroutine(StartCountdown());
+    }
+
+    void movement(float direction)
+    {
+        duration -= Time.deltaTime;
+        Debug.Log(cooldown);
+        float currentDistance = Vector2.Distance(transform.position, destitation);
+        transform.position = Vector2.Lerp(transform.position, destitation, Time.deltaTime + ((currentDistance/ originalDistance) *Time.deltaTime));
+        if(duration <= 0)
+        {
+            isDogding = false;
+            isInvincible = false;
+            playerController.movementPause = false;
+        }
+    }
+
+  
+
 
     public IEnumerator StartCountdown(float countdownValue = 10)
     {
