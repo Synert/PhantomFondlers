ed78ace0523a6271b6ce8cdda3e3b569f4ecc04d
jplant20 <Josephsp@hotmail.co.uk>
2017-10-31 15:33:56 +0000

    Editing the designs of the two menu scenes we have. Will improve upon, but just commiting just in case.

--- BaseProject/Assets/Scripts/DontDestroy.cs
+++ BaseProject/Assets/Scripts/DontDestroy.cs
@@ -3,15 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class DontDestroy : MonoBehaviour {
+public class DontDestroy : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-     
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+        if (GameObject.FindObjectsOfType<DontDestroy>().Length == 1)
+        {
+
+            GameObject.DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SceneManager.GetActiveScene() == SceneManager.GetSceneByName("Level1"))
+        {
+            Destroy(gameObject);
+        }
+    }
 }
