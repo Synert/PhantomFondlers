7c4ea9a5d8afa250598ee364752fcae8a9c1161b
Connor Rhone <syn3rt@gmail.com>
2017-11-01 03:24:46 +0000

    Added player join screen, updated levels with prefabs

---
+++ BaseProject/Assets/Scripts/GetPlayers.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetPlayers : MonoBehaviour {
+
+    PlayerInGame obj;
+    ControllerPoll controllers;
+
+    SpriteRenderer[] selectSprites = new SpriteRenderer[4];
+    SpriteRenderer[] unselectSprites = new SpriteRenderer[4];
+
+
+    // Use this for initialization
+    void Start () {
+        obj = GameObject.Find("PlayerVal").GetComponent<PlayerInGame>();
+        controllers = FindObjectOfType<ControllerPoll>();
+
+        for(int i = 0; i < 4; i++)
+        {
+            selectSprites[i] = GameObject.Find("Player" + (i + 1) + "Select").GetComponent<SpriteRenderer>();
+            unselectSprites[i] = GameObject.Find("Player" + (i + 1) + "Unselect").GetComponent<SpriteRenderer>();
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		//try to listen out for controllers
+        for(int i = 0; i < 4; i++)
+        {
+            if(!controllers.Controllers[i])
+            {
+                obj.playerExists[i] = false;
+            }
+
+            if(obj.playerExists[i])
+            {
+                selectSprites[i].enabled = true;
+                unselectSprites[i].enabled = false;
+            }
+            else
+            {
+                selectSprites[i].enabled = false;
+                unselectSprites[i].enabled = true;
+            }
+        }
+	}
+
+    public void ControllerOne(variableData _var)
+    {
+        if(_var.state.controllerVariables[0] == button.pressed)
+        {
+            obj.playerExists[0] = true;
+        }
+        else if (_var.state.controllerVariables[1] == button.pressed)
+        {
+            obj.playerExists[0] = false;
+        }
+    }
+
+    public void ControllerTwo(variableData _var)
+    {
+        if (_var.state.controllerVariables[0] == button.pressed)
+        {
+            obj.playerExists[1] = true;
+        }
+        else if (_var.state.controllerVariables[1] == button.pressed)
+        {
+            obj.playerExists[1] = false;
+        }
+    }
+
+    public void ControllerThree(variableData _var)
+    {
+        if (_var.state.controllerVariables[0] == button.pressed)
+        {
+            obj.playerExists[2] = true;
+        }
+        else if (_var.state.controllerVariables[1] == button.pressed)
+        {
+            obj.playerExists[2] = false;
+        }
+    }
+
+    public void ControllerFour(variableData _var)
+    {
+        if (_var.state.controllerVariables[0] == button.pressed)
+        {
+            obj.playerExists[3] = true;
+        }
+        else if (_var.state.controllerVariables[1] == button.pressed)
+        {
+            obj.playerExists[3] = false;
+        }
+    }
+}
---
+++ BaseProject/Assets/Scripts/PlayerInGame.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInGame : MonoBehaviour {
+
+    public bool[] playerExists = new bool[4];
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
